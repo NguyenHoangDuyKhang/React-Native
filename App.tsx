b8016aea704e0e4f2aac710c1c491eb3a1e1a2a8
KSBoy <khang12a3lqd@gmail.com>
2024-12-24 21:08:25 +0700

    init project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
+  const [name, SetName] = useState<string>("KSboy");
+  const [age, SetAge] = useState<number>();
+
+  const [Number, SetNumber] = useState<number>(0);
+
+  const handleClick = () => {
+    SetNumber(Number + 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.helo}> {name} Hello</Text>
+      {/* <Text>{JSON.stringify(data)} Hello</Text> */}
+
+      <View>
+        <Button title="btn" onPress={() => handleClick()} />
+        <Text> số nè {Number}</Text>
+
+        <Text>Tên nè {name}</Text>
+        <TextInput
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 100,
+            height: 40,
+          }}
+          onChangeText={(value) => SetName(value)}
+        />
+
+<Text>Tuổi {age}</Text>
+
+        <TextInput
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 100,
+            height: 40,
+            
+          }}
+          keyboardType="numeric"
+          maxLength={2}
+          onChangeText={(value) => SetAge(+value)}
+        />
+      </View>
+
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
+// css in js
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  helo: {
+    color: "blue",
+    fontSize: 25,
+    fontWeight: "bold",
+    padding: 20,
   },
 });
